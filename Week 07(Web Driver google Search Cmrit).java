135b272a8422b3f897cbb7149d9224eb7e82ed57
cheega <156876910+loducmr@users.noreply.github.com>
2024-06-20 10:32:50 +0530

    Update and rename Week 07(Web Driver google Search Cmrit) to Week 07(Web Driver google Search Cmrit).java

--- Week 07(Web Driver google Search Cmrit).java
+++ Week 07(Web Driver google Search Cmrit).java
@@ -5,8 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver; 
 public class Week7 { 
  public static void main(String[] args) throws InterruptedException { 
- System.setProperty("webdriver.chrome.driver", 
-"F:\\Lab\\chromedriver\\chromedriver.exe"); 
+ System.setProperty("webdriver.chrome.driver", "F:\\Lab\\chromedriver\\chromedriver.exe"); 
  WebDriver driver=new ChromeDriver(); 
  driver.manage().window().maximize(); 
  driver.get("https://www.google.com/"); 
